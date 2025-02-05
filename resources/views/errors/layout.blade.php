442b523c14f3d2132ca55c5e2170f146318ba8a8
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-05 13:08:54 +0500

    font changes

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -11,6 +11,9 @@
 
         <!-- Styles -->
         <style>
+            *{
+                font-family: 'Fraunces', serif !important;  
+            }
             html, body {
                 background-color: #fff;
                 color: #636b6f;
